ea70b5e35b377a57877589bfdea387dee12e9c90
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-11 11:47:03 -0400

    cors origin change

--- middleware/cors.go
+++ middleware/cors.go
@@ -5,7 +5,7 @@ import (
 )
 
 func CorsMiddleware(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Access-Control-Allow-Origin", "http://northerndehacks.ca")
+	w.Header().Add("Access-Control-Allow-Origin", "https://northerndehacks.ca")
 
 	w.Header().Add("Access-Control-Allow-Headers", "*")
 }
